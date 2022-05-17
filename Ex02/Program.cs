6256839255b9d8371cafd019ffbfbd2702f16c44
Vasilii <bespalchuk1985@gmail.com>
2022-05-17 11:12:51 +0300

    Ex02

--- Ex02/Program.cs
+++ Ex02/Program.cs
@@ -1,13 +1,14 @@
-﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
-//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; 5,5) Что то тут пошло не так(
+﻿//Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; 5,5)
 Console.Write("Введите b1 ");
-int b1 = Convert.ToInt32(Console.ReadLine());
+double b1 = double.Parse(Console.ReadLine());
 Console.Write("Введите k1 ");
-int k1 = Convert.ToInt32(Console.ReadLine());
+double k1 = double.Parse(Console.ReadLine());
+
 Console.Write("Введите b2 ");
-int b2 = Convert.ToInt32(Console.ReadLine());
+double b2 = double.Parse(Console.ReadLine());
 Console.Write("Введите k2 ");
-int k2 = Convert.ToInt32(Console.ReadLine());
+double k2 = double.Parse(Console.ReadLine());
 
 if (k1 == k2)
 {
@@ -19,9 +20,9 @@ if (k1 == k2)
 }
 else
 {
-    int x = (b2 - b1) / (k1 - k2);
+    double x = (b2 - b1) / (k1 - k2);
     Console.WriteLine("Координаты точки х - " + x);
 
-    int y = k1 * x + b1;
+    double y = k1 * x + b1;
     Console.WriteLine("Координаты точки y - " + y);
 }
